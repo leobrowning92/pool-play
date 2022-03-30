ad19619bceedc77d873fd047342737d87cbe4188
Leo Browning <leo@sportsflare.io>
2022-03-30 14:49:42 +1300

    get player implemented

--- pool/api/views.py
+++ pool/api/views.py
@@ -11,21 +11,44 @@ async def get_all_records(conn):
 
 
 async def add_player(conn, name, rating):
-    # await conn.execute(db.player.insert(), [{"id":2,"name":name, "rating":rating}])
     sql_string = "INSERT INTO player (name, rating) VALUES (%s, %s) RETURNING id;"
     id = await conn.execute(sql_string, (name, rating))
     return await id.fetchone()
+
+
+async def get_player(conn, pid):
+    sql_string = "SELECT * FROM player where id=%s"
+    player = await conn.execute(sql_string, pid)
+    return await player.fetchall()
 
 
 @routes.get("/")
 async def index_handler(request):
     async with request.app["db"].acquire() as conn:
         records = await get_all_records(conn)
-    return web.Response(text=str(records))
+    return web.Response(text=str(records), status=200)
 
 
 @routes.post("/player")
 async def add_player_handler(request):
-    async with request.app["db"].acquire() as conn:
-        add_player(conn, "test", 350)
-        return web.Response(text="success", status=201)
+    try:
+        raw_data = await request.json()
+        async with request.app["db"].acquire() as conn:
+
+            pid = await add_player(conn, raw_data["name"], raw_data["rating"])
+            return web.Response(
+                text=f"success player with {pid[0]} created", status=201
+            )
+    except Exception as e:
+        return web.Response(text=f"{e} for request {request}", status=500)
+
+
+@routes.get("/player")
+async def get_player_handler(request):
+    raw_data = await request.json()
+    try:
+        async with request.app["db"].acquire() as conn:
+            player = await get_player(conn, raw_data["id"])
+            return web.Response(text=str(player), status=200)
+    except Exception as e:
+        return web.Response(text=e, status=500)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 import pytest
 from pool.api.main import setup_app
 from pool.api.settings import CONFIG
-from pool.api.views import get_all_records, add_player
+from pool.api.views import get_all_records, add_player, get_player
 
 
 @pytest.fixture
@@ -31,6 +31,30 @@ async def test_add_player(cli):
 
 
 async def test_add_player_handler(cli):
-    resp = await cli.post("/player")
+    resp = await cli.post("/player", json={"name": "test", "rating": 350})
+    assert await resp.text() == "success player with 1 created"
     assert resp.status == 201
-    assert await resp.text() == "success"
+
+    async with cli.server.app["db"].acquire() as conn:
+        records = await get_all_records(conn)
+        assert records == [(1, "test", 350.0)]
+
+
+@pytest.fixture(scope="function")
+async def single_player_db(cli, test_db):
+    async with cli.server.app["db"].acquire() as conn:
+        await add_player(conn, "test", 350)
+        player = await get_player(conn, 1)
+        assert player == [(1, "test", 350.0)]
+
+
+async def test_get_player(cli, single_player_db):
+    async with cli.server.app["db"].acquire() as conn:
+        player = await get_player(conn, 1)
+        assert player == [(1, "test", 350.0)]
+
+
+async def test_get_player_handler(cli, single_player_db):
+    resp = await cli.get("/player", json={"id": 1})
+    assert await resp.text() == "[(1, 'test', 350.0)]"
+    assert resp.status == 200
